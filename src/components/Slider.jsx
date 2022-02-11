856edfbc3476e2ae537e65d610d77b3fd309a0b4
Stefano Corra <stefano.corra@hotmail.com>
2022-02-10 19:19:19 -0500

    home-page

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
 import styled from "styled-components";
 
@@ -5,7 +6,6 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
 `;
 const Arrow = styled.div`
@@ -19,15 +19,62 @@ const Arrow = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
 `;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+  padding-left: 300px;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1``;
+const Desc = styled.p``;
+const Button = styled.button``;
+
 const Slider = () => {
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left">
         <ArrowLeftOutlined />
       </Arrow>
-      <Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src={require("../images/disruptivo.jpg")} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
+            </Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right">
         <ArrowRightOutlined />
       </Arrow>
     </Container>
