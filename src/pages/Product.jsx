d1eb6447ec8a1c1c5c1c0786d28b0e505889a287
Stefano Corra <stefano.corra@hotmail.com>
2022-02-23 19:56:55 -0500

    filter

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,14 +3,114 @@ import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import { Add, Remove } from "@mui/icons-material";
 
 const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  margin: 30px 0;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+const AddContainer = styled.div``;
+const AmountContainer = styled.div``;
+const Amount = styled.span``;
+const Button = styled.button``;
 
 const Product = () => {
   return (
     <Container>
       <Navbar />
       <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Lorem ipsum</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            laudantium officia, praesentium alias nobis quas expedita vel, eos
+            consequuntur vero, illo enim sed velit quos quasi dignissimos
+            consequatur. Consequatur, quasi.
+          </Desc>
+          <Price>$20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="Black" />
+              <FilterColor color="DarkBlue" />
+              <FilterColor color="Gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
       <Newsletter />
       <Footer />
     </Container>
