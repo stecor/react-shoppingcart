6dcb00e7cdc108a927161ebfd0a1507496aca89d
Stefano Corra <stefano.corra@hotmail.com>
2022-02-26 14:05:23 -0500

    cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,106 @@
+import { Add, Remove } from "@mui/icons-material";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+const Title = styled.div`
+  font-weight: 300;
+  text-align: center;
+`;
+const Top = styled.h1`
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const TopTexts = styled.div``;
+
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0 10px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Info = styled.div`
+  flex: 3;
+`;
+const Summary = styled.div`
+  flex: 1;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 200px;
+`;
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const ProductName = styled.span``;
+const ProductId = styled.span``;
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+const ProductSize = styled.div``;
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
 
 const Cart = () => {
   return (
@@ -12,6 +109,70 @@ const Cart = () => {
       <Announcement />
       <Wrapper>
         <Title>Your Cart</Title>
+        <Top>
+          <TopButton>Continue Shopping</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist (0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">Checkout Now</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1619487269-tr3mmst080-shoe-angle-global-mens-tree-runner-mist-white-b11537e4-5c45-4443-a1dd-c70c31779715-png-1619487255.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> Jessie Thunder shoes
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b>5646944565
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b> 37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://cdn.webshopapp.com/shops/165023/files/217857641/800x1024x2/adidas-adidas-t-shirt-kickboxing-community-gray-bl.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> Adidas T-Shirt
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b>878545456
+                  </ProductId>
+                  <ProductColor color="gray" />
+                  <ProductSize>
+                    <b>Size:</b> M
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 54</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>Summary</Summary>
+        </Bottom>
       </Wrapper>
       <Footer />
     </Container>
