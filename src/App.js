2116f8398ed2c1621401337a29bafef9ea146b88
Stefano Corra <stefano.corra@hotmail.com>
2022-02-26 23:36:52 -0500

    cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+// import Navbar from "./components/Navbar";
+// import Cart from "./pages/Cart";
 //import Login from "./pages/Login";
 //import Register from "./pages/Register";
 //import Product from "./pages/Product";
 //import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Cart />;
+  return <Home />;
 };
 
 export default App;
