a91e6234eb418635943dc2a83fbdd60b7c856604
Stefano Corra <stefano.corra@hotmail.com>
2022-04-06 20:53:56 -0400

    works commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
-// import Home from "./pages/Home";
+import React from 'react'
+import Home from './pages/Home'
 // import Navbar from "./components/Navbar";
- import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
+
+//import Login from './pages/Login'
 // import ProductList from "./pages/ProductList";
 //import Register from "./pages/Register";
 // import Product from "./pages/Product";
 //import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Cart />;
-};
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
 
-export default App;
+export default App
