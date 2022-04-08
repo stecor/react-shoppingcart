e05ce7d7873963c4cdbca9c01534e2c319ba596f
Stefano Corra <stefano.corra@hotmail.com>
2022-04-07 20:31:41 -0400

    arrow commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,9 @@
 // @ts-nocheck
-import React, { useState } from "react";
-import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
-import styled from "styled-components";
-import { sliderItems } from "../data";
-import { mobile } from "../responsive";
+import React, { useState } from 'react'
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material'
+import styled from 'styled-components'
+import { sliderItems } from '../data'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
   width: 100%;
@@ -11,8 +11,8 @@ const Container = styled.div`
   display: flex;
   position: relative;
   overflow: hidden;
-  ${mobile({ display: "none" })};
-`;
+  ${mobile({ display: 'none' })};
+`
 const Arrow = styled.div`
   width: 50px;
   height: 50px;
@@ -24,20 +24,20 @@ const Arrow = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
-  left: ${(props) => (props.direction ? "left" : "10px")};
-  right: ${(props) => props.direction === "right" && "10px"};
+  left: ${(props) => (props.direction ? 'left' : '10px')};
+  right: ${(props) => props.direction === 'right' && '10px'};
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
   z-index: 2;
-`;
+`
 
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
   transition: all 1.5s ease;
   transform: translateX(${(props) => props.slideIndex * -100}vw);
-`;
+`
 
 const Slide = styled.div`
   width: 100vw;
@@ -45,50 +45,50 @@ const Slide = styled.div`
   display: flex;
   align-items: center;
   background-color: #${(props) => props.bg};
-`;
+`
 const ImgContainer = styled.div`
   height: 100%;
   flex: 1;
-`;
+`
 
 const Image = styled.img`
   height: 80%;
   padding-left: 300px;
-`;
+`
 const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
-`;
+`
 
 const Title = styled.h1`
   font-size: 70px;
-`;
+`
 const Desc = styled.p`
   margin: 50px 0px;
   font-size: 20px;
   font-weight: 500;
   letter-spacing: 3px;
-`;
+`
 const Button = styled.button`
   padding: 10px;
   font-size: 20px;
   background-color: transparent;
   cursor: pointer;
-`;
+`
 
 const Slider = () => {
-  const [slideIndex, setSlideIndex] = useState(0);
+  const [slideIndex, setSlideIndex] = useState(0)
   const handleClick = (direction) => {
-    if (direction === "left") {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    if (direction === 'left') {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
     } else {
-      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
     }
-  };
+  }
   return (
     <Container>
-      <Arrow direction="left" onClick={() => handleClick("left")}>
-        <ArrowLeftOutlined />
+      <Arrow direction='left' onClick={() => handleClick('left')}>
+        <ArrowLeftOutlined sx={{ fontSize: 90 }} />
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
@@ -104,11 +104,11 @@ const Slider = () => {
           </Slide>
         ))}
       </Wrapper>
-      <Arrow direction="right" onClick={() => handleClick("right")}>
-        <ArrowRightOutlined />
+      <Arrow direction='right' onClick={() => handleClick('right')}>
+        <ArrowRightOutlined sx={{ fontSize: 90 }} />
       </Arrow>
     </Container>
-  );
-};
+  )
+}
 
-export default Slider;
+export default Slider
