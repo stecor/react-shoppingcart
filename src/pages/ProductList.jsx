01003a4f68115b651c199f788b9aa3e2c204672e
Stefano Corra <stefano.corra@hotmail.com>
2022-06-17 22:47:02 -0400

    commit

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,37 +1,37 @@
-import styled from "styled-components";
-import Announcement from "../components/Announcement";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import Newsletter from "../components/Newsletter";
-import Products from "../components/Products";
-import { mobile } from "../responsive";
+import styled from 'styled-components'
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
+import Products from '../components/Products'
+import { mobile } from '../responsive'
 
-const Container = styled.div``;
+const Container = styled.div``
 const Title = styled.h1`
   margin: 2px;
-`;
+`
 const FilterContainer = styled.div`
   display: flex;
   justify-content: space-between;
-`;
+`
 const Filter = styled.div`
   margin: 20px;
-  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })};
-`;
+  ${mobile({ width: '0px 20px', display: 'flex', flexDirection: 'column' })};
+`
 
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
-  ${mobile({ marginRight: "0px" })};
-`;
+  ${mobile({ marginRight: '0px' })};
+`
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
-  ${mobile({ margin: "10px 0px" })};
-`;
-const Option = styled.option``;
+  ${mobile({ margin: '10px 0px' })};
+`
+const Option = styled.option``
 
 const ProductList = () => {
   return (
@@ -44,7 +44,7 @@ const ProductList = () => {
           <FilterText>Filter Products:</FilterText>
           <Select>
             <Option disabled selected>
-              Color
+              Colors
             </Option>
             <Option>White</Option>
             <Option>Black</Option>
@@ -79,7 +79,7 @@ const ProductList = () => {
       <Newsletter />
       <Footer />
     </Container>
-  );
-};
+  )
+}
 
-export default ProductList;
+export default ProductList
